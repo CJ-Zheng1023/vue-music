9a21f0211c525f5dd351f7f5865c8fef9f06b8a1
AfterWin <CJ_Zheng1023@hotmail.com>
2017-11-14 19:11:28 +0800

    test

--- src/base/slider.vue
+++ src/base/slider.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="container-slider">
-    <slot></slot>
+  <div class="slider" ref="slider">
+    <div class="slider-group" ref="sliderGroup">
+      <slot></slot>
+    </div>
   </div>
+
 </template>
 <script>
+  export default {
+    mounted() {
+      setTimeout(() => {
+        this._setWidth()
+        this._initSlider()
+      }, 20)
+    },
+    methods: {
+      _setWidth() {
+        console.log(this.$refs.sliderGroup.children.length)
+      },
+      _initSlider() {
 
+      }
+    }
+  }
 </script>
+
+<style scoped>
+
+</style>
